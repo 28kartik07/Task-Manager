399207035073b839517bc50f59d830c0f2aa1697
28kartik07 <kartikchandra2000@outlook.com>
2024-11-13 19:57:44 +0530

    Create app layout

--- Task Manager/src/components/Home.jsx
+++ Task Manager/src/components/Home.jsx
@@ -1,10 +1,32 @@
 import '../css/home.css'
 function Home()
 {
+    function handleClick()
+    {
+        console.log("add is clicked");
+    }
     return (
         <div className="home">
-            This is the home component
+
+            <div className="add_task">
+                <button className="add_task_element" onClick={handleClick}>Add Task</button>
+                <button className="add_task_element" onClick={handleClick}>Search Task</button>
+            </div>
+
+            <div className="task_category">
+                <div className="task_element">
+                    <h1>upcoming tasks</h1>
+                </div>
+                <div className="task_element">
+                    <h1>over due tasks</h1>
+                </div>
+                <div className="task_element">
+                    <h1>competed tasks</h1>
+                </div>
+            </div>
+            
         </div>
+
     )
 }
 
